ba6e10ebd1d79d75ddb724187d01b84b67f66b84
Chanaka Karunarathne <kmchmk@gmail.com>
2021-04-02 16:15:09 +0530

    Add more comments and update readme file.

--- README.md
+++ README.md
@@ -11,7 +11,8 @@ pip3 install telnetlib3
 ```sh
 python telnet_client.py <host> <port>
 ```
+- Type "exit" (without quotes) and press Enter to stop the client.
 
 #### Note:
 - When dealing with HTTP protocol, you may have to press Enter twice to let the server know that your request is complete.
-- This client is implemented using [telnetlib3](https://pypi.org/project/telnetlib3/) library.
+- This client is implemented using [telnetlib3](https://pypi.org/project/telnetlib3/) library ([Docs here](https://docs.python.org/3/library/telnetlib.html)).
--- telnet_client.py
+++ telnet_client.py
@@ -1,19 +1,19 @@
 import telnetlib, sys, time
 from threading import Thread
 
-def receive_data(telnetObj):
+def receive_data(telnetObj): # For a separate thread
     while(True):
         try:
-            output = telnetObj.read_eager()
+            output = telnetObj.read_eager() # Read readily available data
             if(output):
                 try:
                     decodedoutput = output.decode('ascii')
                     sys.stdout.write(decodedoutput)
-                except: # Some web contents contain non ascii values.
+                except: # Some web contents contain non ascii values. In that case let's print without decoding
                     sys.stdout.write(str(output))
             else:
                 sys.stdout.flush()
-        except:
+        except: # If some error occured while receiving, this thread should exit without crashing
             print("Connection closed!")
             break
 
@@ -24,20 +24,21 @@ if(len(sys.argv) != 3):
 host = sys.argv[1]
 port = sys.argv[2]
 try:
-    telnetObj = telnetlib.Telnet(host, port, timeout=5)
+    telnetObj = telnetlib.Telnet(host, port, timeout=5) # Initiate
     print("Successfully connected to {}:{}".format(host, port))
-except:
+except: # If incorrect host and port were given or any network issue occured
     print("Error occured while trying to connect {}:{}".format(host, port))
-    sys.exit()
+    sys.exit(1)
 
-thread = Thread(target=receive_data, args=(telnetObj, ))
+thread = Thread(target=receive_data, args=(telnetObj, )) # Create a new thread to listen
 thread.start()
 
 while(True):
     try:
-        command = input()
-        telnetObj.write(command.encode('ascii') + "\n".encode('ascii'))
+        command = input() # Take inputs from user
+        telnetObj.write(command.encode('ascii') + "\n".encode('ascii')) # Send user input to the server
         if(command in ["exit", "quit"] or thread._is_stopped):
+            # If user wanted to exit or if the listening thread is not running the client should stop
             break
     except:
         # print(sys.exc_info()[0])
@@ -45,6 +46,6 @@ while(True):
         break
 
 
-telnetObj.close()
-thread.join()
+telnetObj.close() # Close the telnet socket. This will also make the listening thread exit
+thread.join() # Wait for the listening thread to finish
 print("Finished!")
